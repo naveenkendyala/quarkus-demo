aaad48446d2b6e5bc77711b9ba5201b37c829740
Naveen Kendyala <subscribeken@gmail.com>
2020-08-07 22:21:34 -0400

    updated the setup scripts for the demo

--- todo-app/demo.setup.sh
+++ todo-app/demo.setup.sh
@@ -2,7 +2,11 @@
 oc new-project quarkus-demo
 oc apply -f database/mysql
 
-# Run the database script present in the database.script.sql
-# This needs to be run on the terminal of the MySQL Container
+# Log into the terminal of the mysql pod
+# Execute the command to become root and enter the password
+mysql -u root
+
+# Run the database script present in the "database/mydql/database.script.sql"
 
 # Ready to Start the demo
+./demo.start.sh
