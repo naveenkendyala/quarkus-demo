f7cb9c80630ddc0f3f772a07fc428b18c13c562a
Naveen Kendyala <subscribeken@gmail.com>
2020-08-11 13:54:09 -0400

    Added Springboot to scaling script

--- todo-app/demo.test.scale.sh
+++ todo-app/demo.test.scale.sh
@@ -1,4 +1,4 @@
-#oc scale deployment.apps/springboot-jpa-api-jvm-deploy --replicas=$1
+oc scale deployment.apps/springboot-todo-dc --replicas=$1
 oc scale deployment.apps/quarkus-jvm-todo-dc --replicas=$1
 oc scale deployment.apps/quarkus-native-todo-dc --replicas=$1
 
