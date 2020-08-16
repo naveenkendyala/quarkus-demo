3be5a002aebd0cef0310b50418d195a35af0cea3
Naveen Kendyala <subscribeken@gmail.com>
2020-08-15 23:13:17 -0400

    Added ToDo list for updating the setup

--- todo-app/demo.setup.sh
+++ todo-app/demo.setup.sh
@@ -7,7 +7,14 @@ oc apply -f database/mysql
 # The password is "demo"
 mysql -u root
 
+# TODO
+# Update the instruction to log into the mysql from command line
+
 # Run the database script present in the "database/mydql/database.script.sql"
 
 # Ready to Start the demo
 ./demo.start.sh
+
+# TODO
+# Include instructions for the testing
+# Include instructions for the scaling
