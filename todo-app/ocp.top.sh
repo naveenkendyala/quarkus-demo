dafedf3ffec46518e4bb2ae29914eee59f3521d2
Naveen Kendyala <subscribeken@gmail.com>
2020-09-16 21:19:22 -0400

    Added the icons to the deployments and updated the top command echo statements

--- todo-app/ocp.top.sh
+++ todo-app/ocp.top.sh
@@ -11,9 +11,9 @@ do
  #Fix for the Kubectl top pod command & sort on memory
  echo Metrics Refresh Every $1 seconds............
  echo ""
- echo "####################################################################"
- echo "POD NAME	 			         CPU   MEMORY             "
- echo "####################################################################"
+ echo "#####################################################"
+ echo "POD NAME		 	       CPU   MEMORY     "
+ echo "#####################################################"
  oc adm top po --no-headers | sort --reverse --key 3 --numeric | head -100
 
  #Sleep for the dashboard to display
