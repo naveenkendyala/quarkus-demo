66606ab958775f9eae4d31dcab197cbb26ceec06
Naveen Kendyala <subscribeken@gmail.com>
2022-05-02 17:32:22 -0400

    Quarkus 2.2 uodate

--- todo-app/java/quarkus-api/build-jvm.sh
+++ todo-app/java/quarkus-api/build-jvm.sh
@@ -1,13 +1,16 @@
 #/bin/bash
+#Quarkus Version
+VERSION=2.2
+
 ./mvnw clean package -Dquarkus-profile=jvm
 
 echo "*****************************************************************************"
-docker rmi quarkus-demo/quarkusjvm-todo:v1
-docker rmi quay.io/naveenkendyala/quarkus-demo-quarkusjvm-todo:v1;
-docker build -f src/main/build/docker/Dockerfile.jvm -t quarkus-demo/quarkusjvm-todo:v1 .; 
+docker rmi quarkus-demo/quarkusjvm-todo:${VERSION}
+docker rmi quay.io/naveenkendyala/quarkus-demo-quarkusjvm-todo:${VERSION};
+docker build -f src/main/build/docker/Dockerfile.jvm -t quarkus-demo/quarkusjvm-todo:${VERSION} .;
 
 echo "*****************************************************************************"
-docker tag quarkus-demo/quarkusjvm-todo:v1 quay.io/naveenkendyala/quarkus-demo-quarkusjvm-todo:v1;
+docker tag quarkus-demo/quarkusjvm-todo:${VERSION} quay.io/naveenkendyala/quarkus-demo-quarkusjvm-todo:${VERSION};
 
 echo "*****************************************************************************"
-docker push quay.io/naveenkendyala/quarkus-demo-quarkusjvm-todo:v1;
+docker push quay.io/naveenkendyala/quarkus-demo-quarkusjvm-todo:${VERSION};
--- todo-app/java/quarkus-api/build-native.sh
+++ todo-app/java/quarkus-api/build-native.sh
@@ -1,13 +1,16 @@
 #/bin/bash
+#Quarkus Version
+VERSION=2.2
+
 ./mvnw clean package -Pnative -Dquarkus.profile=native -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker
 
 echo "*****************************************************************************"
-docker rmi quarkus-demo/quarkusnative-todo:v1
-docker rmi quay.io/naveenkendyala/quarkus-demo-quarkusnative-todo:v1
-docker build -f src/main/build/docker/Dockerfile.native -t quarkus-demo-quarkusnative-todo:v1 .; 
+docker rmi quarkus-demo/quarkusnative-todo:${VERSION}
+docker rmi quay.io/naveenkendyala/quarkus-demo-quarkusnative-todo:${VERSION}
+docker build -f src/main/build/docker/Dockerfile.native -t quarkus-demo-quarkusnative-todo:${VERSION} .;
 
 echo "*****************************************************************************"
-docker tag quarkus-demo-quarkusnative-todo:v1 quay.io/naveenkendyala/quarkus-demo-quarkusnative-todo:v1; 
+docker tag quarkus-demo-quarkusnative-todo:${VERSION} quay.io/naveenkendyala/quarkus-demo-quarkusnative-todo:${VERSION}; 
 
 echo "*****************************************************************************"
-docker push quay.io/naveenkendyala/quarkus-demo-quarkusnative-todo:v1
+docker push quay.io/naveenkendyala/quarkus-demo-quarkusnative-todo:${VERSION}
