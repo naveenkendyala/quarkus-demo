f3c1aff4eb2653fd0aaa0fe8694d79874be06318
Naveen Kendyala <subscribeken@gmail.com>
2023-06-13 12:16:57 -0400

    Added lines for native image specify builder

--- todo-app/java/quarkus-api/build-native.sh
+++ todo-app/java/quarkus-api/build-native.sh
@@ -4,6 +4,10 @@ VERSION=v2.13.7
 
 ./mvnw clean package -Pnative -Dquarkus.profile=native -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=podman
 
+#Overriding native image builder
+#./mvnw clean package -Pnative -Dquarkus.native.container-build=true -Dquarkus.native.builder-image=quay.io/quarkus/ubi-quarkus-mandrel-builder-image:22.3-java17
+#./mvnw clean package -Pnative -Dquarkus.profile=native -Dquarkus.native.container-build=true -Dquarkus.native.builder-image=quay.io/quarkus/ubi-quarkus-mandrel-builder-image:22.3.2.1-Final-java17
+
 echo "*****************************************************************************"
 podman rmi quarkus-demo/quarkusnative-todo:${VERSION}
 podman rmi quay.io/naveenkendyala/quarkus-demo-quarkusnative-todo:${VERSION}
